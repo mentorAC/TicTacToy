0c57098d8bf37cd77a1cce113421355fd240b9d4
maxpe3447 <121520054+mentorAC@users.noreply.github.com>
2023-12-04 18:33:04 +0200

    add macroses

--- TicTacToy/Source.cpp
+++ TicTacToy/Source.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+#define COLOR_BLACK 0
+#define COLOR_RED 4
+#define MOVING << 4
+#define WHITE_COLOR 7
+#define UP 72
+#define DOWN 80
+#define RIGHT 77
+#define LEFT 75
+#define ENTER '\r'
+#define CROSS 'x'
+#define CIRCLE 'o'
+#define GREEN_COLOR 2
+
 using namespace std;
 
 int main() {
